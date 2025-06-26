ef043d05dfb87b89633f84017da68fa89aad3a7d
Timur <astominsflavadept@gmail.com>
2025-06-26 15:10:09 +0500

    added antd

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,6 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold">Url Shortener</h1>
-  <div class="mt-4"></div>
+  <h1 class="text-3xl font-bold">Url shortener</h1>
+  <form class="mt-4">
+    <a-space>
+      <a-input placeholder="Enter url" />
+      <a-button type="primary">Shorten</a-button>
+    </a-space>
+  </form>
 </template>
