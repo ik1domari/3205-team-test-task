7756f4989fb03ae6070702b19ec65b31fdaa09ab
Timur <astominsflavadept@gmail.com>
2025-06-26 13:30:35 +0500

    vue router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import Home from "./pages/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 
 const app = createApp(App);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+app.use(autoAnimatePlugin);
+app.use(router);
 
 app.mount("#app");
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,2 @@
+<script lang="ts" setup></script>
+<template><h1>Home</h1></template>
